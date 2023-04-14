fb25902824b488c761dd08017d7431efee8d9c90
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-14 10:47:32 +0200

    createComment endpoint

---
+++ server/Auth/createComment.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const createComment = express.Router();
+const user = require("./User")
+const {Post} = require("./createPost")
+
+
+createComment.post("/", async (req, res) => {
+    const userID = req.session.userId
+    const postID = req.body.id 
+    const comment = req.body.comment 
+    const username = await user.find({_id: `${userID}`}).lean().select('username').exec()
+    console.log(username[0].username)
+    try {
+       
+        const post = await Post.updateOne({_id: `${postID}`}, {$push: {comments: {comment: `${comment}`, username: `${username[0].username}`}}})
+        res.send(post)
+    }
+    catch(err){
+        console.log(err)
+    }
+
+
+})
+
+module.exports = createComment
