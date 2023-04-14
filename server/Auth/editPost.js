a2bed22ee629e0c5e3c2a79841a48084912d7895
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-14 10:48:14 +0200

    Edit post endpoint

---
+++ server/Auth/editPost.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const editPost = express.Router();
+const {Post} = require("./createPost")
+
+editPost.patch("/", async (req, res) => {
+ const userID = req.session.userId
+ const postID = req.body.id 
+ const newText = req.body.text
+ const checkIfUserIsAuthToEdit = await Post.findOne({_id: `${postID}`, userId: `${userID}`})
+ 
+ if(!checkIfUserIsAuthToEdit){
+    return res.sendStatus(401)
+ }
+try {
+    const uptade = await Post.findByIdAndUpdate(postID, {post: `${newText}`})
+   res.sendStatus(200)
+
+}catch(err){
+    console.log(err)
+}
+})
+
+module.exports = editPost
