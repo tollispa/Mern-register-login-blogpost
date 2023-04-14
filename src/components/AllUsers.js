ec86b5c0543cc3c0dcbe56e8db693fb948cde127
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-14 10:50:46 +0200

    Fetching all users and mapping them out

---
+++ src/components/AllUsers.js
@@ -0,0 +1,25 @@
+import {useState, useEffect} from "react"
+import axios from "axios"
+import {Link} from "react-router-dom"
+
+const AllUsers = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:4000/users")
+        .then((res) => {
+            setUsers(res.data)
+        })
+    },[])
+    console.log(users)
+ return (
+    <div className="hidden border-2 relative border-black rounded p-8 text-left font-bold sm:block">
+      
+        {users.length === 0 ? <p>No users registered, go to <Link to="/login">Register</Link></p> : users.map((user) => {
+            return <p className="m-2"><Link className=""to={`/feed/${user._id}`}>{user.username.toUpperCase()}</Link></p>
+        })}
+    </div>
+ )
+}
+
+export default AllUsers
