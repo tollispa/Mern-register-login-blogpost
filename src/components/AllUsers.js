ec6f76afdb26c3125745fafdf396b4c54a110872
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-16 10:20:01 +0200

    Changed text so i can see if db isnt running

--- src/components/AllUsers.js
+++ src/components/AllUsers.js
@@ -15,7 +15,7 @@ const AllUsers = () => {
  return (
     <div className="hidden border-2 relative border-black rounded p-8 text-left font-bold sm:block">
       
-        {users.length === 0 ? <p>No users registered, go to <Link to="/login">Register</Link></p> : users.map((user) => {
+        {users.length === 0 ? <p>Database isn't running yet.</p> : users.map((user) => {
             return <p className="m-2"><Link className=""to={`/feed/${user._id}`}>{user.username.toUpperCase()}</Link></p>
         })}
     </div>
