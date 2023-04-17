87308378a13bd14312b0639ec401cdfe2fbd4bcb
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-17 21:04:43 +0200

    added Avatar to userSchema as empty string

--- server/schemas/User.js
+++ server/schemas/User.js
@@ -8,6 +8,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String,
+        required: false
     }
 },  { strict: "throw" });
 
