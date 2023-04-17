ee8ca06c1f3b2de8ec0e51e8f7f16bfaa11abdc7
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-17 16:21:30 +0200

    removed unecessary files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import './index.css'
+import './App.css';
 import Login from './components/Login';
 import  {Register}  from './components/Register';
 import AddItem from "./components/AddItems";
