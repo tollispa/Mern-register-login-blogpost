174b412d92a276f73cf24e0776eefae97b33ffe8
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-17 21:04:05 +0200

    User gets a default avatar picture

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,11 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import axios from "axios"
+import {useEffect, useState} from "react"
 import {useNavigate} from "react-router-dom"
 
 function MyNav() {
-
+  const [avatarPic, setAvatarPic] = useState("")
   const navigate = useNavigate()
   const logout = () => {
       axios.get("http://localhost:4000/logout")
@@ -15,8 +16,17 @@ function MyNav() {
         navigate("/")
       })
   }
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/isLoggedIn")
+    .then((res) => {
+      if(res.data === "") {
+       setAvatarPic("https://villagesonmacarthur.com/wp-content/uploads/2020/12/Blank-Avatar.png")
+      }
+    }).catch((err) => console.log(err))
+}, [])
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className='relative' bg="light" expand="lg">
       <Container>
         <Navbar.Brand className="text-white-"href="/">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -25,8 +35,12 @@ function MyNav() {
             <Nav.Link href="/login">Login</Nav.Link>
             
             <Nav.Link href="/feed">Feed</Nav.Link>
+           
+            
+            <NavDropdown title={<img className='w-full h-[25px] inline-block rounded-full object-contain' src={`${avatarPic}`} alt=""/>} id="basic-nav-dropdown">
 
-            <NavDropdown title="Account" id="basic-nav-dropdown">
+
+              
               <NavDropdown.Item href="#action/3.1">Settings</NavDropdown.Item>
               <NavDropdown.Item href="/add-item">
                 Add Item
