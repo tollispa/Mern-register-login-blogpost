c04cd1b3dbe923fab1396cc14626babc1cf86851
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-17 21:53:37 +0200

    Fixed and styled my Avatar component

---
+++ src/components/Avatars.js
@@ -0,0 +1,34 @@
+import axios from "axios"
+import {useState, useEffect} from "react"
+
+const Avatars = () => {
+    const [avatars, setavatars] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:4000/avatars")
+        .then((res) => {
+            setavatars(res.data)
+        })
+    },[])
+
+    const selectAvatar = (URL) => {
+        console.log(URL)
+    }
+    return (
+        <div className="flex flex-col items-center">
+        <h1 className="text-2xl font-bold mb-4">Select your Avatar</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {avatars.map((avatar) => {
+            return (
+              <div key={avatar._id}className="rounded-lg overflow-hidden shadow-lg">
+                <img onClick={() => selectAvatar(avatar.avatar_url)} className="h-48 w-full object-cover" src={avatar.avatar_url} alt={avatar.avatar_alt} />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      
+    )
+}
+
+export default Avatars
