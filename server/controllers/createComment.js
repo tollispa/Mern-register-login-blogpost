1f06dc67bd160d5d76327c7ffa43fe21b10283d6
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-17 17:47:34 +0200

    added so if no logged in user comment = 404status

--- server/controllers/createComment.js
+++ server/controllers/createComment.js
@@ -8,8 +8,11 @@ createComment.post("/", async (req, res) => {
     const userID = req.session.userId
     const postID = req.body.id 
     const comment = req.body.comment 
+    if(!userID) {
+       return res.sendStatus(400)
+    }
     const username = await user.find({_id: `${userID}`}).lean().select('username').exec()
-    console.log(username[0].username)
+    
     try {
        
         const post = await Post.updateOne({_id: `${postID}`}, {$push: {comments: {comment: `${comment}`, username: `${username[0].username}`}}})
